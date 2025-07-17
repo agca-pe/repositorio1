ecfc9305a4eb64f6524c11b1bc757da6187ee469
agca-pe <angela.campos@unmsm.edu.pe>
2025-07-16 21:12:49 -0500

    mi primer día

---
+++ tipos de datos.py
@@ -0,0 +1,11 @@
+"string"
+'string'
+'''tus datos son
+Angela 
+Campos'''
+"""tus datos son
+Angela 
+Campos"""
+40 "entero INT"
+40.2 "flotante FLOAT"
+False "o True: booleano BOOL"
---
+++ variables.py
@@ -0,0 +1,16 @@
+a=5
+b=8
+c=a+b
+print(c)
+nombre="Angela"
+print(nombre)
+numero=7
+numero+=5
+print(numero)
+concatenar="Hola "+ nombre+ " ¿cómo estás?"
+print(concatenar)
+bienvenida1="hola {numero} cómo estás"
+bienvenida2=f"hola {numero} cómo estás"
+print(bienvenida1)
+del numero #Elimina la variable, en este caso no será visible porque se agregó después de declarar la variable bienvenida
+print(bienvenida2)
